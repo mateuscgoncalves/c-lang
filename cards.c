54a7c0b6cfdee902289a4b659dae7d11a0fa88b5
Mateus Gonçalves <mateus_c_g@hotmail.com>
2021-04-14 16:51:34 -0300

    while and conditions

--- cards.c
+++ cards.c
@@ -8,31 +8,40 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
 
-    int val = 0;
+    int count = 0;
 
-    switch (card_name[0]) {
-        case 'K':
-        case 'Q':
-        case 'J':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        default:
-            val = atoi(card_name);
-    }
+    while(card_name[0] != 'X'){
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
 
-    printf("The card value is: %i\n", val);
+        int val = 0;
 
-    /*Check if value is 3 to 6 */
-    if(val >= 3 && val <=6){
-        puts("Count has gone up");
-    }else if(val == 10){
-        puts("Count has gone down");
+        switch (card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            default:
+                val = atoi(card_name);
+                if((val < 1) || (val > 10)){
+                    puts("The value should be bigger than 1 and smaller than 10");
+                    continue;
+                }
+        }
+
+        /*Check if value is 3 to 6 */
+        if(val >= 3 && val <=6){
+            count++;
+        }else if(val == 10){
+            count--;
+        }
+
+        printf("Current count: %i\n", count);
     }
 
     return 0;
