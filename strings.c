f75198a16bd8a37e7e1f3a7aec7a19fd37d2f2c7
Mateus Gonçalves <mateus_c_g@hotmail.com>
2021-04-18 19:43:50 -0300

    added some comments

--- strings.c
+++ strings.c
@@ -7,10 +7,19 @@
 
 void fortune_cookie(char msg[]){
     printf("Message reads: %s\n", msg);
+
+    //Exibe o sizeof do ponteiro
     printf("msg occupies %i bytes\n", sizeof(msg));
 }
 
 int main(){
     char quote[] = "Cookies make you fat";
+
+    //Passa o ponteiro da variavel quote
     fortune_cookie(quote);
+
+    //C entende que nesse caso queremos o sizeof da array, não só do ponteiro
+    printf("quote occupies %i bytes\n", sizeof(quote));
+
+    printf("The quote string is stored at: %p\n", quote);
 }
